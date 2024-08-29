c6d97d9769ee1a8fab6084f449aad31d35321d9e
FOCKUSTY <116993925+FOCKUSTY@users.noreply.github.com>
2024-08-29 09:49:00 +0500

    hotfix

--- Database/src/types.ts
+++ Database/src/types.ts
@@ -4,4 +4,5 @@ export { logAttributes } from './schemas/moongoose/db.log';
 export { multiplatformAttributes as multiplatformType } from './schemas/moongoose/db.mutliplatform';
 export { settedCreatorsVoiceChannelsAttributes } from './schemas/moongoose/db.settedCVC';
 export { Language, dbLanguageAttributes } from './schemas/moongoose/db.locale';
+
 export * from './m.index';
