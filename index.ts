f396edc71f2a50a8d57f4be55adc583ee1407aaa
FOCKUSTY <wwddwwwd@mail.ru>
2025-01-17 20:25:20 +0500

    bug fixed

--- index.ts
+++ index.ts
@@ -1,13 +1,8 @@
 import * as database from "./database/index";
 import * as loaders from "./loaders/index";
-
 import * as ollama from "./ollama/index";
 
 export = {
-	...ollama,
-	...database,
-	...loaders,
-
 	ollama,
 	database,
 	loaders
