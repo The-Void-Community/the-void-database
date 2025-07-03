351a5f5fb33371165fdd8f8642530d04ee6081c0
fockusty <wwddwwwd@mail.ru>
2025-07-03 14:17:17 +0500

    -hot-fix

--- src/types/config.type.ts
+++ src/types/config.type.ts
@@ -1,9 +1,6 @@
 import type { Message as DiscordMessage } from "discord.js";
 
-export type Optional<
-  Main,
-  Default = null
-> = Main | Default;
+export type Optional<Main, Default = null> = Main | Default;
 
 export type ConfigurableMessage = {
   message: Optional<DiscordMessage<true>, string>;
@@ -17,29 +14,32 @@ export interface IConfig {
 
     when_user_leave_from_guild_send_message_to_user: ConfigurableMessage;
     when_user_join_into_guild_send_hello_message_to_channel: ConfigurableMessage;
-  
+
     when_user_leave_from_guild_send_goodbye_message_to_channel: ConfigurableMessage;
     when_user_join_into_guild_grant_roles: ConfigurableMessage;
+
+    /** channel id */
+    whenUserJoinIntoVoiceCreateVoiceAndMoveHim: string;
   };
 
   logging: {
     when_bot_join_into_guild_send_log_into_channel: Optional<string>;
     when_bot_leave_from_guild_send_log_into_channel: Optional<string>;
-    
+
     when_user_join_into_guild_send_log_into_channel: Optional<string>;
     when_user_leave_from_guild_send_log_into_channel: Optional<string>;
-    
+
     when_message_was_sended_send_log_into_channel: Optional<string>;
     when_message_was_changed_send_log_into_channel: Optional<string>;
     when_message_was_deleted_send_log_into_channel: Optional<string>;
-    
+
     when_user_change_profile_send_log_into_channel: Optional<string>;
     when_user_change_activity_send_log_into_channel: Optional<string>;
     when_user_takes_mute_send_log_into_channel: Optional<string>;
     when_user_takes_ban_send_log_into_channel: Optional<string>;
-    
+
     when_roles_changes_at_user_send_log_into_channel: Optional<string>;
-    
+
     when_guild_profile_changes_send_log_into_channel: Optional<string>;
   };
 }
